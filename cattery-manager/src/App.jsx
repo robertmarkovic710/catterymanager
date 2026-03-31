3675aeb3a2801027c76b8d21b11409e6493aede7
rmarkovic70 <rmarkovic70@gmail.com>
2026-03-31 19:15:33 +0200

    create cat and cat list features added

--- cattery-manager/src/App.jsx
+++ cattery-manager/src/App.jsx
@@ -1,11 +1,90 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import Header from "./components/Header";
 
 function App() {
-  const [cats, setCats] = useState([]);
+
+  const [cats, setCats] = useState(() => {
+    const saved = localStorage.getItem("cats");
+    return saved ? JSON.parse(saved) : [];
+  });
+
+  const [name, setName] = useState("");
+  const [breed, setBreed] = useState("");
+  const [age, setAge] = useState("");
+
+  useEffect(() => {
+    localStorage.setItem("cats", JSON.stringify(cats));
+  }, [cats]);
+
+  const addCat = () => {
+
+    if (name === "" || breed === "" || age === "") {
+      alert("Popuni sva polja!");
+      return;
+    }
+
+    const newCat = {
+      id: Date.now(),
+      name: name,
+      breed: breed,
+      age: age
+    };
+
+    setCats([...cats, newCat]);
+
+    setName("");
+    setBreed("");
+    setAge("");
+  };
 
   return (
-    <div>
-      <h1>Welcome to my Cattery Manager homepage.</h1>
+    <div className="container">
+      
+      <Header title="Cattery Manager" />
+
+      <div className="form-container">
+        
+        <input
+          type="text"
+          placeholder="Ime"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+
+        <input
+          type="text"
+          placeholder="Pasmina"
+          value={breed}
+          onChange={(e) => setBreed(e.target.value)}
+        />
+
+        <input
+          type="number"
+          placeholder="Godine"
+          value={age}
+          onChange={(e) => setAge(e.target.value)}
+        />
+
+        <button onClick={addCat}>
+          Dodaj mačku
+        </button>
+
+      </div>
+
+      {cats.length === 0 ? (
+        <p className="empty-message">
+          Nemaš još unesenih mačaka. <br /> Dodaj mačku da započneš!
+        </p>
+       ) : (
+        cats.map((cat) => (
+          <div key={cat.id} className="cat-card">
+            <p><b>Ime:</b> {cat.name}</p>
+            <p><b>Pasmina:</b> {cat.breed}</p>
+            <p><b>Godine:</b> {cat.age}</p>
+          </div>
+        ))
+      )}
+
     </div>
   );
 }
---
+++ cattery-manager/src/components/Header.jsx
@@ -0,0 +1,15 @@
+function Header() {
+    return (
+        <div style= {{
+            backgroundColor: "#333",
+            color: "white",
+            padding: "15px",
+            marginBottom: "20px"
+        }}>
+            <h1>My first cattery manager</h1>
+            <p>Cats are awesome!</p>
+        </div>
+    );
+}
+
+export default Header;
